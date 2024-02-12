ca478a5d72e8db6b0834f0bd7c066f8078643ba4
Rohit Choudhary <111062895+Rohit-Choudhary133@users.noreply.github.com>
2024-02-12 23:11:20 +0530

    Add files via upload

---
+++ PrimeNum.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+    // int p = 0;
+    // int i = 0;
+
+    int primes[50] = {0};
+    int primeIndex = 2;
+
+    bool isPrime = false;
+
+    // hardcode prime numbers
+    primes[0] = 2;
+    primes[1] = 3;
+
+    for(int p = 5; p <= 100; p = p + 2)
+    {
+        isPrime = true;
+
+        for (int i = 1; isPrime && p / primes[i] >= primes[i]; ++i)
+            if (p % primes[i] == 0)
+                isPrime = false;
+
+        if (isPrime == true)
+        {
+            primes[primeIndex] = p;
+            ++primeIndex;
+        }
+    }
+
+    for (int i = 0;  i < primeIndex;  ++i )
+         printf ("%i  ", primes[i]);
+
+    printf("\n");
+    return 0;
+}
